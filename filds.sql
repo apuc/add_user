b63069a7c800e4a630c1d4ce0ab6363d7c2492c9
Korol Dima <korol_dima@list.ru>
2015-06-30 13:40:40 +0300

    Poisk i paginaciya

--- filds.sql
+++ filds.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: phototim.mysql.ukraine.com.ua
--- Время создания: Июн 24 2015 г., 11:40
+-- Время создания: Июн 30 2015 г., 13:38
 -- Версия сервера: 5.1.72-cll-lve
 -- Версия PHP: 5.2.17
 
@@ -31,6 +31,7 @@ CREATE TABLE IF NOT EXISTS `filds` (
   `key` varchar(100) CHARACTER SET cp1251 NOT NULL,
   `label` varchar(100) CHARACTER SET cp1251 NOT NULL,
   `priority` int(5) NOT NULL,
+  `publication` int(2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=27 ;
 
@@ -38,19 +39,19 @@ CREATE TABLE IF NOT EXISTS `filds` (
 -- Дамп данных таблицы `filds`
 --
 
-INSERT INTO `filds` (`id`, `key`, `label`, `priority`) VALUES
-(12, 'first_name', 'Имя', 1),
-(13, 'user_login', 'Логин', 2),
-(14, 'user_pass', 'Пароль', 3),
-(26, 'user_email', 'Email', 4),
-(16, 'name_komp', 'Имя компании', 5),
-(17, 'telephone', 'Номер телефона', 6),
-(18, 'inn', 'ИНН', 7),
-(19, 'lic', 'Лицевой счет', 8),
-(20, 'nomdogovor', 'Номер договора', 9),
-(21, 'date_dog', 'Дата договора', 10),
-(22, 'adres', 'Адрес', 11),
-(23, 'date_rozd', 'Дата рождения', 12);
+INSERT INTO `filds` (`id`, `key`, `label`, `priority`, `publication`) VALUES
+(12, 'first_name', 'Имя', 1, 1),
+(13, 'user_login', 'Логин', 2, 0),
+(14, 'user_pass', 'Пароль', 3, 0),
+(26, 'user_email', 'Email', 4, 0),
+(16, 'name_komp', 'Имя компании', 5, 0),
+(17, 'telephone', 'Номер телефона', 6, 0),
+(18, 'inn', 'ИНН', 7, 0),
+(19, 'lic', 'Лицевой счет', 8, 0),
+(20, 'nomdogovor', 'Номер договора', 9, 1),
+(21, 'date_dog', 'Дата договора', 10, 0),
+(22, 'adres', 'Адрес', 11, 0),
+(23, 'date_rozd', 'Дата рождения', 12, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
